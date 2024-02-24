c809613405184aad068ff490ace44b79c4a593f9
Robin Hood <robinhood70@live.ca>
2024-02-24 16:55:16 -0500

    Move non-trail stuff to Riven

--- BreadCrumb.i18n.magic.php
+++ BreadCrumb.i18n.magic.php
@@ -3,11 +3,6 @@
 $magicWords = [];
 
 $magicWords['en'] = [
-	'CORENAME' => [0, 'CORENAME'],
-	'SORTABLECORENAME' => [0, 'SORTABLECORENAME'],
-	'LABELNAME' => [0, 'LABELNAME'],
-	'sortable' => [0, 'sortable'],
-	'label' => [0, 'label'],
 	'inittrail' => [0, 'inittrail'],
 	'settrail' => [0, 'settrail'],
 	'addtotrail' => [0, 'addtotrail'],
